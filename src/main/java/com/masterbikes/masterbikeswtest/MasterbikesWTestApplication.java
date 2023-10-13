8efa4d2e958957dac52167f8cfecb41fbe9ef388
ignacio-leon-m <ig.leon@duocuc.cl>
2023-10-13 16:21:19 -0300

    first commit

---
+++ src/main/java/com/masterbikes/masterbikeswtest/MasterbikesWTestApplication.java
@@ -0,0 +1,13 @@
+package com.masterbikes.masterbikeswtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterbikesWTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterbikesWTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/masterbikes/masterbikeswtest/MasterbikesWTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masterbikes.masterbikeswtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterbikesWTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
